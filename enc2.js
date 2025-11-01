2196ba0ce3f8e4c96031539ce4bc1922984f7c99
Krishn-2822 <krishnupadhyay2822@gmail.com>
2025-11-01 12:07:44 +0530

    index page

--- enc2.js
+++ enc2.js
@@ -6,13 +6,17 @@ let age = 18;
 console.log(nanme)
 console.log(age)
 
-console.log(nanme , age)
-console.table([nanme,age])
+ 
 
-console.log(typeof age)
+ console.log(typeof age)
 console.log(typeof nanme)
 
 let a = 10;
 let b = '10';
 
 console.log(a===b)
+
+console.log(`my name is ${nanme} and my age is ${18}`)
+
+let newstr = `My name is ${nanme} and my age is ${18}`
+console.log("newstr : ",newstr)
---
+++ enc3.js
@@ -0,0 +1,6 @@
+let nme = "Krishn"
+
+console.log(nme[0])
+
+console.log(nme.toUpperCase())
+console.log(nme.toLowerCase())
