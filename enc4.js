287776741a684887ebe444555fae50c7d6075b06
Krishn-2822 <krishnupadhyay2822@gmail.com>
2025-11-01 12:47:11 +0530

    Function and array push

---
+++ enc4.js
@@ -0,0 +1,25 @@
+let Arr = [1,2.346,"Ridham",[1,2,3]]
+
+console.log(Arr[2])
+
+Arr.push(12)
+Arr.push(13)
+Arr.push(14)
+Arr.push(15)
+Arr.push(16)
+Arr.push(17)
+Arr.push(18)
+Arr.push(19)
+
+function sayhello()
+{
+    console.log("Hello world")
+}
+
+sayhello();
+
+let add = function addition(a,b)
+{
+    return a+b;
+}
+console.log(add(10,5))
