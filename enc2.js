a6590d68570e02f990bd5c740b2487ea7686174e
Krishn-2822 <krishnupadhyay2822@gmail.com>
2025-11-01 11:46:53 +0530

    first commit

---
+++ enc2.js
@@ -0,0 +1,18 @@
+//console.log("hello world")
+
+let nanme = "Krishn";
+let age = 18;
+
+console.log(nanme)
+console.log(age)
+
+console.log(nanme , age)
+console.table([nanme,age])
+
+console.log(typeof age)
+console.log(typeof nanme)
+
+let a = 10;
+let b = '10';
+
+console.log(a===b)
